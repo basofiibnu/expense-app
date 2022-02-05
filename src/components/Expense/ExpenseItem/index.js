48369a72a9ebcff485519a6d5741c3cfae8e1be7
basofiibnu <>
2022-02-05 09:39:54 +0700

    added prettier settings

--- src/components/Expense/ExpenseItem/index.js
+++ src/components/Expense/ExpenseItem/index.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import Card from '../../UI/Card';
-import ExpenseDate from '../ExpenseDate';
-import './styles.css';
+import React, { useState } from "react";
+import Card from "../../UI/Card";
+import ExpenseDate from "../ExpenseDate";
+import "./styles.css";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
+  const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle(Math.random());
-        console.log('Clicked')
-    }
+  const clickHandler = () => {
+    setTitle(Math.random());
+    console.log("Clicked");
+  };
 
-    return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
-    );
-}
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
